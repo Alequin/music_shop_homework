dd770f5b7722228f5ac12c9934588d650219bbbe
James Cox <j-cox03@hotmail.com>
2017-06-30 22:01:49 +0100

    Instrument: made variable access private

--- instrument/Instrument.java
+++ instrument/Instrument.java
@@ -5,11 +5,15 @@ import shop.Sellable;
 
 public abstract class Instrument extends Sellable implements Playable{
 
-  protected InstrumentType type;
+  private InstrumentType type;
 
   public Instrument(double tradePrice, double sellPrice, String description, InstrumentType type){
     super(tradePrice, sellPrice, description);
     this.type = type;
+  }
+
+  public InstrumentType getType(){
+    return this.type;
   }
 
 }
