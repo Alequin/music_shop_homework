b2ee2598fc52dbb7423496aa7e74432ce529b41b
James Cox <j-cox03@hotmail.com>
2017-06-30 22:23:16 +0100

    Flute: Made flute class

---
+++ instrument/Flute.java
@@ -0,0 +1,32 @@
+package instrument;
+
+public class Flute extends Instrument{
+
+  private int lengthInCentimeters;
+  private boolean openHoles;
+
+  public Flute(int lengthInCentimeters, boolean openHoles,
+  double tradePrice, double sellPrice, String description){
+    super(tradePrice, sellPrice, description, InstrumentType.WIND);
+    if(lengthInCentimeters <= 0){
+      throw new IllegalArgumentException(
+        "lengthInCentimeters must be greated than 0\n" +
+        "lengthInCentimeters: " + lengthInCentimeters
+      );
+    }
+    this.lengthInCentimeters = lengthInCentimeters;
+    this.openHoles = openHoles;
+  }
+
+  public int getLengthInCentimeters(){
+    return this.lengthInCentimeters;
+  }
+
+  public int isOpenHoles(){
+    return this.openHoles;
+  }
+
+  public String play(){
+    return "whistle whistle";
+  }
+}
