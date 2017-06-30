00412338614482a668df8fbe2a02ab10b7706c57
James Cox <j-cox03@hotmail.com>
2017-06-30 21:29:09 +0100

    Sellable: made constructor

--- shop/Sellable.java
+++ shop/Sellable.java
@@ -6,6 +6,12 @@ public abstract class Sellable{
   private double tradePrice;
   private double sellPrice;
 
+  public Sellable(double tradePrice, double sellPrice, String description){
+    this.tradePrice = tradePrice;
+    this.sellPrice = sellPrice;
+    this.description = description;
+  }
+
   public double calculateMarkUp(){
     // Find the difference between
     return 0.0;
