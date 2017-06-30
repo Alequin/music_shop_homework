935cceb065c4df6963e787a1cf1804a5e0fec44a
James Cox <j-cox03@hotmail.com>
2017-06-30 21:15:27 +0100

    Made Sellable abstract class. Changed variale name to tradePrice

---
+++ shop/Sellable.java
@@ -0,0 +1,19 @@
+package shop;
+
+public abstract class Sellable{
+
+  private String description;
+  private double tradePrice;
+  private double sellPrice;
+
+  public double calculateMarkUp(){
+    // Find the difference between
+  }
+
+  public String getDescription();
+
+  public double getTradePrice();
+
+  public double getSellPrice();
+
+}
