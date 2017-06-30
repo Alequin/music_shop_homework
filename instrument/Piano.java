d5bdca3a4b7ec70bd6a8f4a2f192a157df599e44
James Cox <j-cox03@hotmail.com>
2017-06-30 22:09:14 +0100

    Piano: Made piano class

---
+++ instrument/Piano.java
@@ -0,0 +1,25 @@
+package instrument;
+
+public class Piano extends Instrument{
+
+  private int keys;
+  private int pedals;
+
+  public Piano(int keys, int pedals, double tradePrice, double sellPrice, String description){
+    super(tradePrice, sellPrice, description, InstrumentType.STRING);
+    this.keys = keys;
+    this.pedals = pedals;
+  }
+
+  public int getKeysCount(){
+    return this.keys;
+  }
+
+  public int getPedalsCount(){
+    return this.pedals;
+  }
+
+  public String play(){
+    return "E, D, C, D, E, E, E, D, D, D, E, E, E";
+  }
+}
